648971d30b2a7f48cec6b71c4111381af2aa7ec2
Aleksnadar <aleksandar.perisic@outlook.com>
2019-05-03 19:21:02 +0200

    new

--- pages/registration.popup.js
+++ pages/registration.popup.js
@@ -2,6 +2,9 @@ let regPage = function () {
     this.lefttopimg=element.all(by.css('div[class="gigya-brand gigya-brand-sbs6-tv-web"]')).first();
     this.lefttoptxt=element.all(by.css('div[class="title"]')).first();
     this.
+    this.leftmidbottomtxt=element.all(by.css('div[class="bottom"] span')).first();
+    this.leftmidbottomlink=element.all(by.css('div[class="bottom"] a')).first();
+    this.
 
 };
 module.exports = regPage;
